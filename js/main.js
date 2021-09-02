bedd8d02bf8b5d03f31c15dea0d6a0019d98f1e0
Mostofareza3 <86896410+Mostofareza3@users.noreply.github.com>
2021-09-02 09:43:49 +0600

    third commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const searchInput = document.getElementById('searchInput');
 const searchButton = document.getElementById('search-button');
-const booksContainer = document.getElementById('books-container')
+const booksContainer = document.getElementById('books-container');
+const totalFound = document.getElementById('total-found');
 
 searchButton.addEventListener('click', function(){
     const search = searchInput.value;
@@ -14,21 +15,21 @@ searchButton.addEventListener('click', function(){
 const displayData = books =>{
     // console.log(books.length)
     books.forEach(book => {
-        console.log(book)
         const div = document.createElement('div');
         div.classList.add('col')
         div.innerHTML =`
         <div class="card h-100">
-        <img src="https://covers.openlibrary.org/b/id/554106-M.jpg" class="card-img-top" alt="...">
+        <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">Book : ${book.title}</h5>
-          <p class="card-text">Author : ${book.author_name[0]}</p>
-          <p class="card-text">Publisher : ${book.publisher[0]}</p>
+          <p class="card-text">Author : ${book.author_name}</p>
+          <p class="card-text">Publisher : ${book.publisher}</p>
           <p class="card-text">First publish : ${book.first_publish_year}</p>
         </div>
       </div>
       `
-      booksContainer.appendChild(div)
+      booksContainer.appendChild(div);
+      totalFound.innerText = `Total Found :${books.length}`;
         
     })
 }
